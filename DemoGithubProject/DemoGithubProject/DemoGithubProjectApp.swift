fee7e979026deb4ce46946c81778fb420d1f3ce7
bizcyben <Benchaphon500103@gmail.com>
2024-06-23 10:04:09 +0700

    firstcommit

---
+++ DemoGithubProject/DemoGithubProject/DemoGithubProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoGithubProjectApp.swift
+//  DemoGithubProject
+//
+//  Created by Benchaphon Phukhen on 23/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct DemoGithubProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
